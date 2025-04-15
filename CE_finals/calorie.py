3f5e9b8ffca516212cdf0b41e93077ccd660005c
Stephanie Martinez <stephnez09@gmail.com>
2025-04-15 15:08:30 +0800

    Add files via upload

---
+++ CE_finals/calorie.py
@@ -0,0 +1,9 @@
+
+calories_per_minute = 4.2
+
+minutes = 10
+
+while minutes <= 30:
+    calories_burned = minutes * calories_per_minute
+    print(f"Calories burned after {minutes} minutes: {calories_burned:}")
+    minutes += 5  
