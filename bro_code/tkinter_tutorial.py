5ac910c4552c7fe8da4f4c35b7e0c43b2c79357e
Tep <stephnez09@gmail.com>
2025-05-07 22:15:59 +0800

    Add files via upload

---
+++ bro_code/tkinter_tutorial.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import tkinter as tk
+from PIL import Image, ImageTk
+
+window = tk.Tk()
+window.geometry("420x420")
+window.title("First GUI")
+window.config(background="black")
+
+# image = Image.open('IMG_20240802_185948_613.jpg')
+# icon = ImageTk.PhotoImage(image)
+# window.iconphoto(True, icon)
+
+window.mainloop()
